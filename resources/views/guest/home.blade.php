14dbc1ef47a595d327ccd4088b89e65efb4eaee2
SirEdDy1 <42296241+SirEdDy1@users.noreply.github.com>
2019-09-22 22:53:10 +0700

    change2

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -20,6 +20,27 @@
             object-fit: cover;
             object-position: center;
         }
+        .picbox{
+            width: 200px;
+            height: 150px;
+            overflow: hidden;
+            justify-content: center;
+            align-items: center;
+        }
+        .picbox > img{
+            width: 80%;
+            height: 80%;
+            object-fit: cover;
+            object-position: center;
+        }
+        .nopadding {
+            padding: 0 !important;
+            margin: 0 !important;
+        }
+        .other{
+            padding:auto;
+            padding-top: 3rem;
+        }
     </style>
 @endpush
 
@@ -30,12 +51,30 @@
         </div>
         <div class="features col-12 row">
             <div class="col row">
-                <div class="col">col</div>
-                <div class="col">col</div>
-                <div class="col">col</div>
+                <div class="col">
+                    <div class="row"><h5>Lorem ipsum dolor sit amet.</h5></div>
+                    <div class="row picbox"><img src="{{URL::asset('/pic/TranPhu.jpg')}}" alt=""></div>
+                    <div class="row">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, in.</div>
+                </div>
+                <div class="col">
+                    <div class="row"><h5>Lorem ipsum dolor sit amet.</h5></div>
+                    <div class="row picbox"><img src="{{URL::asset('/pic/TranPhu.jpg')}}" alt=""></div>
+                    <div class="row">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, in.</div>
+                </div>
+                <div class="col">
+                    <div class="row"><h5>Lorem ipsum dolor sit amet.</h5></div>
+                    <div class="row picbox"><img src="{{URL::asset('/pic/TranPhu.jpg')}}" alt=""></div>
+                    <div class="row">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, in.</div>
+                </div>
             </div>
-            <div class="col">col</div>
-
+        </div>
+        <div class="other">
+            <h5>Tin tiêu điểm</h5>
+            <ul>
+                <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium, maxime.</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat earum doloremque sit ex praesentium recusandae?</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam.</li>
+            </ul>
         </div>
     </div>
 @endsection
