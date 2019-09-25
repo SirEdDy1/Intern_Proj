53c974b09b65a0b4d575a4cd0923f1e8abe8c2d1
SirEdDy1 <42296241+SirEdDy1@users.noreply.github.com>
2019-09-25 12:21:05 +0700

    more changes

--- resources/views/guest/schedule.blade.php
+++ resources/views/guest/schedule.blade.php
@@ -2,12 +2,71 @@
 
 @push('custom-css')
     <style>
-        h1 {
-            color: red;
+        .container{
+            max-width: 100%;
+            max-height: 100%;
+        }
+        header{
+            background: #343A40;
+            color: white;
+            padding: 5px
+
+        }
+        ul {
+            list-style: none; /* Remove list bullets */
+            padding: 0;
+            margin: 0;
+        }
+        li {
+             clear: both;
+        }
+
+        li:before {
+            content: ""; /* Insert content that looks like bullets */
+        }
+        .box{
+            padding: 10px;
+        }
+        .box > p{
+            font-size: 70%;
         }
     </style>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 @endpush
 
 @section('content')
-    <h1>This is schedule Page</h1>
+
+    <div class="container m-0">
+      <div class="row">
+          <div class="col">
+              <h4>Giới thiệu</h4>
+              <ul>
+                  <li><i class="material-icons">menu_book</i>Lorem ipsum dolor sit amet.</li>
+                  <li><i class="material-icons">menu_book</i>Lorem ipsum dolor sit amet.</li>
+                  <li><i class="material-icons">menu_book</i>Lorem ipsum dolor sit amet.</li>
+              </ul>
+              <h4>Văn bản</h4>
+              <ul>
+                  <li><i class="material-icons">menu_book</i>Lorem ipsum dolor sit amet.</li>
+                  <li><i class="material-icons">menu_book</i>Lorem ipsum dolor sit amet.</li>
+                  <li><i class="material-icons">menu_book</i>Lorem ipsum dolor sit amet.</li>
+              </ul>
+          </div>
+          <div class="col-5">
+              <div class="box">
+                    <h5>Lorem ipsum dolor sit amet.</h5>
+                    <p>Ngày 25/09/2019</p>
+                    <a href="">file.docx</a>
+              </div>
+          </div>
+          <div class="col">
+              <h4>Liên hệ</h4>
+              <ul>
+                  <li><i class="material-icons">menu_book</i>Lorem ipsum dolor sit amet.</li>
+                  <li><i class="material-icons">menu_book</i>Lorem ipsum dolor sit amet.</li>
+                  <li><i class="material-icons">menu_book</i>Lorem ipsum dolor sit amet.</li>
+              </ul>
+          </div>
+      </div>
+  </div>
 @endsection
